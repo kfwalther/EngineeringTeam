219f4ad6c4e4c726418e5b147b7e0d455ec57aae
Kevin Walther <kfwalther@gmail.com>
2016-07-12 21:19:20 -0400

    Adding some Doxygen-compatible comments to main file and parser class header for reference.

--- WheelOfJeopardy/WheelOfJeopardy/WheelOfJeopardycpp.cpp
+++ WheelOfJeopardy/WheelOfJeopardy/WheelOfJeopardycpp.cpp
@@ -1,4 +1,9 @@
-
+/**
+ * @file WheelOfJeopardycpp.cpp
+ * @brief This file contains the main function.
+ * @author Edwin Flores
+ * @date 2016
+ */
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -6,14 +11,23 @@
 #include "questions.h"
 #include "parser.h"
 
+// Alias the std namespace for convenience.
 using namespace std;
 
+/**
+ * @brief This is the main entry point to the WoJ program.
+ */
 void main()
 {
+	// Define a queue container of question objects.
 	queue <questions> Q;
+	// Define the category input file to use.
 	string c = "category1.csv";
+	// Define the parser object.
 	parser Parse;
+	// Call the parser for the given category.
 	Parse.parseCategory(Q, c);
+	// Print some output.
 	cout << c << " " << Q.size() << endl;
 }
 
--- WheelOfJeopardy/WheelOfJeopardy/parser.h
+++ WheelOfJeopardy/WheelOfJeopardy/parser.h
@@ -1,11 +1,26 @@
 #pragma once
 #include <queue>
 #include "questions.h"
+
+/**
+ * @class parser
+ * @brief WoJ Parser object.
+ * @author Edwin Flores
+ * @date 2016
+ * @ingroup WheelOfJeopardy
+ */
 class parser
 {
 public:
 	parser();
 	~parser();
+
+	/**
+	 * @brief Parse and store the question information for the given category.
+	 *
+	 * @param[in] category A string indicating the category input file.
+	 * @param[in,out] queue A queue container housing questions objects.
+	 */
 	void parseCategory(std::queue<questions> &queue, std::string &category);
 };
 

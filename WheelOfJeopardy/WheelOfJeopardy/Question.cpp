e7adcf81e7d1445a2bcb44ab878023ce8fb1b006
Kevin Walther <kfwalther@gmail.com>
2016-08-12 18:15:14 -0400

    Update Question.cpp
    editting Question class to print the remaining time in seconds

--- WheelOfJeopardy/WheelOfJeopardy/Question.cpp
+++ WheelOfJeopardy/WheelOfJeopardy/Question.cpp
@@ -7,7 +7,7 @@
 #include "Question.h"
 #include <string>
 #include <iostream>//testcode
-
+#include <algorithm>
 
 Question::Question()
 {
@@ -57,13 +57,12 @@ void Question::setQuestion(std::string q)
 bool Question::checkAnswer(std::string const & playerAnswer)
 {
 	// TODO: Make this a little more robust, account for capitalizations, etc...
-	std::time_t endTime = std::time(nullptr);
-	if (playerAnswer == this->answerContent && endTime-startTime > this->timer) {
-		// TODO: Convert time printed here to seconds.
-		std::cout << "Correct! Time remaining was: " << endTime - startTime << std::endl;//testcode
+	double const elapsedTime = std::difftime(std::time(nullptr), this->startTime);
+	if ((playerAnswer == this->answerContent) && (elapsedTime < this->timer)) {
+		std::cout << "Correct! Time remaining was: " << std::max(0.0, (this->timer - elapsedTime)) << " seconds" << std::endl;//testcode
 		return true;
 	} else {
-		std::cout << "Wrong! Correct answer was: " << this->answerContent<< std::endl;//testcode
+		std::cout << "Wrong! Correct answer was: " << this->answerContent << std::endl;//testcode
 		this->pointValue *= -1;	//if answered incorrectly, point value is now negative
 		return false;
 	}
